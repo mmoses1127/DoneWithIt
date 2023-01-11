307298b72d5eaac619fe1d709ca0b4e6b3ea69cb
Michael <mmoses1127@gmail.com>
2023-01-11 14:56:11 -0800

    add image, basic press fxns, text

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableNativeFeedback, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('text pressed');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello world!</Text>
+      <TouchableNativeFeedback onLongPress={() => console.log('image tapped')}>
+        <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
